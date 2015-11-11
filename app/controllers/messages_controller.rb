f400d53c8b1ee84d5cb881bf7780dcb126824c9e
kohtomoda <info@oj9.co.jp>
2015-11-11 13:52:37 +0000

    create destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,33 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def destroy
+    @message.destroy
+      redirect_to root_path , notice: 'メッセージを削除しました'
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else 
+      #保存に失敗した場合は編集画面へ移す
+      render 'edit'
+    end
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+  
   def index
     @messages = Message.all
     @message = Message.new
-    end
+  end
     
   def create
     @message = Message.new(message_params)
