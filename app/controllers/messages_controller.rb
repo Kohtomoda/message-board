ee1b6fb89e58b17750c95213cb1943d369bbc307
kohtomoda <info@oj9.co.jp>
2015-11-11 19:12:04 +0000

    add age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,7 @@ class MessagesController < ApplicationController
   
   
   def index
+    # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
@@ -32,18 +33,18 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path , notice: 'メッセージを保存しました'
     else
-      #メッセージが保存できなかった時
+      # メッセージが保存できなかった時
       @messages = Message.all
-      flash.now[:alert] = "ちょ、ちょっと！メッセージが短かすぎです！"
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
       render 'index'
     end
   end
   
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   
 end
---
+++ db/migrate/20151111164239_add_to_age_messages.rb
@@ -0,0 +1,5 @@
+class AddToAgeMessages < ActiveRecord::Migration
+  def change
+        
+  end
+end
