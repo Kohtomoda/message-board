9ed830ffd046a7e333a8279c5319bacbc9839327
kohtomoda <info@oj9.co.jp>
2015-11-15 05:18:06 +0000

    kadai-shusei

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151111174847) do
 
-  create_table "massages", force: :cascade do |t|
-    t.string   "name"
-    t.string   "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
